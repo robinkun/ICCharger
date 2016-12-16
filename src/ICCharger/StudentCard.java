9ecbe367e2b1582dba813b565a549a7de29f14cd
Riku Sato <r163333@edu.tut.ac.jp>
2016-12-16 17:35:47 +0900

    studentNumberをint型からString型に変更した

--- src/ICCharger/StudentCard.java
+++ src/ICCharger/StudentCard.java
@@ -2,6 +2,6 @@ package ICCharger;
 
 public class StudentCard {
 	private String studentName; // 学生名
-	private int studentNumber; // 学籍番号
+	private String studentNumber; // 学籍番号
 	private int accountBalance; // 残高
 }
