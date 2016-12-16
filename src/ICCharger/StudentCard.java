1f3388afe36f1a45ed83afadacd2faeed4ab9ad6
Riku Sato <r163333@edu.tut.ac.jp>
2016-12-16 17:43:04 +0900

    各変数のゲッターとセッターを作った

--- src/ICCharger/StudentCard.java
+++ src/ICCharger/StudentCard.java
@@ -4,4 +4,28 @@ public class StudentCard {
 	private String studentName; // äwê∂ñº
 	private String studentNumber; // äwê–î‘çÜ
 	private int accountBalance; // écçÇ
+	
+	void setStudentName(String name) {
+		studentName = name;
+	}
+	
+	String getStudentName() {
+		return studentName;
+	}
+	
+	void setStudentNumber(String number) {
+		studentNumber = number;
+	}
+	
+	String getStudentNumber() {
+		return studentNumber;
+	}
+	
+	void setAccountBalance(int money) {
+		accountBalance = money;
+	}
+	
+	int getAccountBalance() {
+		return accountBalance;
+	}
 }
