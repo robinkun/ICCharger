d76b44df758016a227bcfb82622fc3c4fe8e051a
Riku Sato <r163333@edu.tut.ac.jp>
2016-12-16 17:34:11 +0900

    残高の変数名をAccountBalanceに変えた

--- src/ICCharger/StudentCard.java
+++ src/ICCharger/StudentCard.java
@@ -3,5 +3,5 @@ package ICCharger;
 public class StudentCard {
 	private String studentName; // 学生名
 	private int studentNumber; // 学籍番号
-	private int balance; // 残高
+	private int accountBalance; // 残高
 }
