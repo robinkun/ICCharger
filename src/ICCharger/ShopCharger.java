62b7b3ff6e9eb30a792d8e5145bde83b49f85fde
Riku Sato <r163333@edu.tut.ac.jp>
2016-12-21 17:44:21 +0900

    チャージ履歴を表示するメソッドを作った

--- src/ICCharger/ShopCharger.java
+++ src/ICCharger/ShopCharger.java
@@ -20,13 +20,23 @@ public class ShopCharger {
 			finalChargeDate.poll();
 		}
 		
+		int difference = insertedCard.chargeAccountBalance(money);
 		ChargeHistory history = new ChargeHistory(insertedCard.getStudentNumber(), insertedCard.getAccountBalance());
 		finalChargeDate.offer(history);
-		return insertedCard.chargeAccountBalance(money);
+		return difference;
 	}
 	
 	void printAccountBalance() {
 		System.out.printf("学籍番号：%6d, 名前：%s, 残高%d円\n",
 				insertedCard.getStudentNumber(), insertedCard.getStudentName(), insertedCard.getAccountBalance());
+	}
+	
+	void printChargeHistory() {
+		Iterator<ChargeHistory> i = finalChargeDate.iterator();
+		
+		while(i.hasNext()) {
+			ChargeHistory ch = (ChargeHistory)i.next();
+			System.out.printf("id:%d, Date:%s, balance:%d\n", ch.getId(), ch.getDate().getTime(), ch.getBalance());
+		}
 	}
 }
