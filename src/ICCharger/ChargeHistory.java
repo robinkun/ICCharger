ea8d2c5aba5fb86386e8fd74da41fa2d827836b0
Riku Sato <r163333@edu.tut.ac.jp>
2016-12-21 17:32:41 +0900

    各変数のゲッターを作った

--- src/ICCharger/ChargeHistory.java
+++ src/ICCharger/ChargeHistory.java
@@ -2,12 +2,24 @@ package ICCharger;
 import java.util.Calendar;
 
 public class ChargeHistory {
-	int id = 0;
-	Calendar date;
-	int balance = 0;
+	private int id = 0;
+	private Calendar date;
+	private int balance = 0;
 	
 	ChargeHistory(int id, int balance) {
 		this.id = id;
 		this.balance = balance;
+	}
+	
+	int getId() {
+		return id;
+	}
+	
+	Calendar getDate() {
+		return date;
+	}
+	
+	int getBalance() {
+		return balance;
 	}
 }
