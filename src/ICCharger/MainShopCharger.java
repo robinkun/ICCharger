0fd0373a23291819c65364f92863f4dcc41557fa
Riku Sato <r163333@edu.tut.ac.jp>
2016-12-21 15:28:07 +0900

    中身をShopCharger.javaに移動

--- src/ICCharger/MainShopCharger.java
+++ src/ICCharger/MainShopCharger.java
@@ -2,6 +2,17 @@ package ICCharger;
 
 public class MainShopCharger {
 	public static void main(String[] args) {
-		System.out.println("uoo");
+		StudentCard card1, card2;
+		card1 = new StudentCard(163000, "‚à‚¿‚à‚¿‚Ì–Ø ˜a‹`");
+		card2 = new StudentCard(129888, "‰^ê•ë ƒ\ƒƒ‚ƒ“");
+		ShopCharger mainShopCharger = new ShopCharger();
+		
+		mainShopCharger.insertStudentCard(card1);
+		mainShopCharger.chargeMoney(3000);
+		mainShopCharger.printAccountBalance();
+		
+		mainShopCharger.insertStudentCard(card2);
+		mainShopCharger.chargeMoney(8765);
+		mainShopCharger.printAccountBalance();
 	}
 }
