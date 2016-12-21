c8a6d76f143ec13555ccd22f852bfe85b1877942
Riku Sato <r163333@edu.tut.ac.jp>
2016-12-21 17:24:13 +0900

    実装を書いた

--- src/ICCharger/ChargeHistory.java
+++ src/ICCharger/ChargeHistory.java
@@ -1,5 +1,13 @@
 package ICCharger;
+import java.util.Calendar;
 
 public class ChargeHistory {
-
+	int id = 0;
+	Calendar date;
+	int balance = 0;
+	
+	ChargeHistory(int id, int balance) {
+		this.id = id;
+		this.balance = balance;
+	}
 }
